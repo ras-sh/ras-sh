e4106338ebe30ab508073b5141d2397c44f3ed34
Richard Solomou <richard@solomou.io>
2025-09-23 08:46:26 +0300

    add redirect

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   typedRoutes: true,
   transpilePackages: ["@ras-sh/ui"],
+  redirects() {
+    return [
+      {
+        source: "/convex-cli",
+        destination: "https://www.npmjs.com/package/@ras-sh/convex-cli",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
